33646f748dd0c889584f1592c4392dfad6265f90
Gideon <ghoffman2032@norfolkcollegiate.org>
2026-09-24 16:40:12 -0400

    lego

--- eva1.py
+++ eva1.py
@@ -9,10 +9,10 @@ from base_robot import *
 def Run(br: BaseRobot):
     # Wait until the team is ready and the robot is in its starting position.
     br.driveForDistance(
-        distance=650,
+        distnce=650,
         speedPct=100,
         then=Stop.BRAKE,
-        waiting=True,
+        waitng=True,
     )
 
     br.driveForDistance(
